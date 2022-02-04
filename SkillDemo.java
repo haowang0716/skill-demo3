ff586b1753334c4cf3dfdfab2a3065dcdbc88124
haowang0716 <haw044@ucsd.edu>
2022-02-04 00:22:07 -0800

    create skilldemo file and a fail case

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo {
+    public static int minus(int x, int y) {
+        return x - y; 
+    }
+}
---
+++ SkillDemoTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.minus(20, 3));
+    }
+}
